c071d75aff930f9fb052d47cfc7aa9dbeb944437
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2020-07-28 17:46:38 -0500

    probando

--- rest-api-node-mysql/src/database.js
+++ rest-api-node-mysql/src/database.js
@@ -10,12 +10,31 @@ var dbconfig ={
 };
 const mysqlConnection = mysql.createConnection(dbconfig);
 
-
+var connection;
+function handleDisconnect() {
+    connection = mysql.createConnection(dbconfig);  // Recreate the connection, since the old one cannot be reused.
+    connection.connect( function onConnect(err) {   // The server is either down
+        if (err) {                                  // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 10000);    // We introduce a delay before attempting to reconnect,
+        }                                           // to avoid a hot loop, and to allow our node script to
+    });                                             // process asynchronous requests in the meantime.
+                                                    // If you're also serving http, display a 503 error.
+    connection.on('error', function onError(err) {
+        console.log('db error', err);
+        if (err.code == 'PROTOCOL_CONNECTION_LOST') {   // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                        // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
 mysqlConnection.connect(function (err) {
     if(err){
         console.log(err);
         return;
     }else{
+        handleDisconnect()
         console.log("Db is connected");
     }
 });
