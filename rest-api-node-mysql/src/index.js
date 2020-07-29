4711a398656abfee20f76a80a09753a1c2dee298
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2020-07-28 22:18:50 -0500

    trying to solve problems

--- rest-api-node-mysql/src/index.js
+++ rest-api-node-mysql/src/index.js
@@ -4,14 +4,6 @@ var cors = require('cors');
 const wakeDyno = require("woke-dyno");
 //Settings
 //Solo para pushear xd
-app.listen(process.env.PORT, () => {
-    wakeDyno({
-        url: "https://redflag-api.herokuapp.com/usuario/1",  // url string
-        interval: 20000, // interval in milliseconds (1 minute in this example)
-        startNap: [3, 0, 0, 0], // the time to start nap in UTC, as [h, m, s, ms] (05:00 UTC in this example)
-        endNap: [7, 00, 00, 000] // time to wake up again, in UTC (09:59:59.999 in this example)
-    }).start(); 
-});
 
 app.set('port', process.env.PORT || 3000); //Acepta el puerto que nos da el servidor en caso que no te de uda el 3000
 
@@ -32,3 +24,12 @@ app.use(require('./routes/reportes'));
 app.listen(app.get('port'), () => {
     console.log('Server on port', app.get('port'));
 } );
+
+app.listen(process.env.PORT, () => {
+    wakeDyno({
+        url: "https://redflag-api.herokuapp.com/usuario/1",  // url string
+        interval: 20000, // interval in milliseconds (1 minute in this example)
+        startNap: [3, 0, 0, 0], // the time to start nap in UTC, as [h, m, s, ms] (05:00 UTC in this example)
+        endNap: [7, 00, 00, 000] // time to wake up again, in UTC (09:59:59.999 in this example)
+    }).start(); 
+});
