f1e651ab6e78577cf0cf579f5ac1282dc0ccdda3
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2020-07-29 14:07:55 -0500

    Putting down cors

--- rest-api-node-mysql/src/index.js
+++ rest-api-node-mysql/src/index.js
@@ -33,7 +33,7 @@ app.listen(port, () => {
     wakeDyno({
         url: "https://redflag-api.herokuapp.com/usuario/1",  // url string
         interval: 20000, // interval in milliseconds (1 minute in this example)
-        startNap: [19, 0, 0, 0], // the time to start nap in UTC, as [h, m, s, ms] (05:00 UTC in this example)
+        startNap: [24, 0, 0, 0], // the time to start nap in UTC, as [h, m, s, ms] (05:00 UTC in this example)
         endNap: [03, 00, 00, 000] // time to wake up again, in UTC (09:59:59.999 in this example)
     }).start(); 
 });
