84e33f3bf086e44e7a53b9a8898cc45775ab4ff5
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2020-07-28 22:12:06 -0500

    añadido un hilo para que no caiga el server

--- rest-api-node-mysql/src/index.js
+++ rest-api-node-mysql/src/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const  app = express();
 var cors = require('cors');
+const wakeDyno = require("woke-dyno");
 //Settings
 //Solo para pushear xd
+app.listen(process.env.PORT || 3000, () => {
+    wakeDyno({
+        url: "https://redflag-api.herokuapp.com/usuario/1",  // url string
+        interval: 20000, // interval in milliseconds (1 minute in this example)
+        startNap: [3, 0, 0, 0], // the time to start nap in UTC, as [h, m, s, ms] (05:00 UTC in this example)
+        endNap: [7, 00, 00, 000] // time to wake up again, in UTC (09:59:59.999 in this example)
+    }).start(); 
+});
+
 app.set('port', process.env.PORT || 3000); //Acepta el puerto que nos da el servidor en caso que no te de uda el 3000
 
 //Middlewares
