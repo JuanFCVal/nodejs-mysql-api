ec52ecb9da088ed579049ffa23a7dff417bdc479
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2020-07-28 22:34:34 -0500

    Maybe correct port

--- rest-api-node-mysql/src/index.js
+++ rest-api-node-mysql/src/index.js
@@ -20,12 +20,13 @@ app.use(require('./routes/donacion'));
 app.use(require('./routes/usuario'));
 app.use(require('./routes/reportes'));
 
-//Starting server
-app.listen(app.get('port'), () => {
-    console.log('Server on port', app.get('port'));
-} );
+//Starting server/*
+//app.listen(app.get('port'), () => {
+    
+//} );
 
 app.listen(port, () => {
+    console.log('Server on port', app.get('port'));
     wakeDyno({
         url: "https://redflag-api.herokuapp.com/usuario/1",  // url string
         interval: 20000, // interval in milliseconds (1 minute in this example)
