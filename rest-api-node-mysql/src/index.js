ad948fc85812a57f1bff9a5d61c1ee82855afec0
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2020-07-31 11:38:59 -0500

    aa

--- rest-api-node-mysql/src/index.js
+++ rest-api-node-mysql/src/index.js
@@ -39,11 +39,11 @@ app.use(require('./routes/reportes'));
 
 app.listen(port, () => {
     console.log('Server on port', app.get('port'));
-    /*
+    
     wakeDyno({
         url: "https://redflag-api.herokuapp.com/usuario/1",  // url string
         interval: 20000, // interval in milliseconds (1 minute in this example)
         startNap: [12, 0, 0, 0], // the time to start nap in UTC, as [h, m, s, ms] (05:00 UTC in this example)
         endNap: [13, 00, 10, 000] // time to wake up again, in UTC (09:59:59.999 in this example)
-    }).start(); */
+    }).start(); 
 });
