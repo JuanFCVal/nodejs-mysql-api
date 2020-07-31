1d8c7004374b18d71cfd3ad31e02137958b46ca4
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2020-07-30 20:51:36 -0500

    donacion validas

--- rest-api-node-mysql/src/routes/donacion.js
+++ rest-api-node-mysql/src/routes/donacion.js
@@ -23,6 +23,16 @@ router.get("/donacion/pendiente", (req, res) => {
   }); 
 });
 
+router.get("/donacion/valido", (req, res) => {
+  mysqlConnection.query("SELECT * FROM donacion where estado=1", (err, rows, fields) => {//Sentencia y lo que podemos obtener
+    if (!err) {
+      res.json(rows);
+    } else {
+      console.log("No se pudo obtener la lista donaciones " + err);
+    }
+  }); 
+});
+
 //Ver los tipos de donaciones de una donacion
 router.get("/donacion/:id/donaciones", (req, res) => {
   const { id } = req.params;
